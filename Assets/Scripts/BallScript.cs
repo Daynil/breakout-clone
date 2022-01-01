d866ffffdb75833e7a92e79f01bebbe2dd868a62
Danny Libin <dlibinrx@gmail.com>
2022-01-01 11:59:53 -0600

    bricks and damage

--- Assets/Scripts/BallScript.cs
+++ Assets/Scripts/BallScript.cs
@@ -8,13 +8,13 @@ public class BallScript : MonoBehaviour
   public Vector2 velocity;
   public Vector2 startForce;
 
-  private Rigidbody2D rb2d;
+  private Rigidbody2D rb;
 
   // Start is called be1fore the first frame update
   void Start()
   {
-    this.rb2d = this.GetComponent<Rigidbody2D>();
-    this.rb2d.AddForce(startForce);
+    this.rb = this.GetComponent<Rigidbody2D>();
+    this.rb.AddForce(startForce);
   }
 
   // Update is called once per frame
@@ -22,7 +22,7 @@ public class BallScript : MonoBehaviour
   {
   }
 
-  public void OnTriggerEnter2D(Collider2D other)
+  void OnTriggerEnter2D(Collider2D other)
   {
     if (other.name == "Floor")
     {
@@ -31,8 +31,18 @@ public class BallScript : MonoBehaviour
     }
   }
 
+  void OnCollisionEnter2D(Collision2D collision)
+  {
+    if (collision.collider.tag == "Brick")
+    {
+      collision.collider.GetComponent<BrickScript>().Damage(1);
+    }
+  }
+
   private void ResetBall()
   {
     this.transform.position = new Vector3(0.41f, 2.33f, 0);
+    this.rb.velocity = Vector2.zero;
+    this.rb.AddForce(startForce);
   }
 }
---
+++ Assets/Scripts/BrickScript.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrickScript : MonoBehaviour
+{
+
+  public int health;
+  public bool indestructable;
+  SpriteRenderer sr;
+
+  // Start is called before the first frame update
+  void Start()
+  {
+    this.sr = this.GetComponent<SpriteRenderer>();
+    this.SetColor();
+  }
+
+  private void SetColor()
+  {
+    if (this.indestructable)
+    {
+      this.sr.color = Color.black;
+      return;
+    }
+
+    if (this.health == 3)
+    {
+      this.sr.color = new Color(0, 255, 0);
+    }
+    else if (this.health == 2)
+    {
+      this.sr.color = new Color(0, 150, 150);
+    }
+    else
+    {
+      this.sr.color = new Color(255, 0, 0);
+    }
+  }
+
+  public void Damage(int amount)
+  {
+    if (this.indestructable) return;
+
+    this.health = this.health - amount;
+    if (this.health <= 0)
+    {
+      Destroy(this.gameObject);
+    }
+    else
+    {
+      this.SetColor();
+    }
+  }
+}
